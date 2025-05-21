7b28875afd3ae57b3ea4f6410473829b6a4a7d58
Zahrin Savana <khadijahzahrinsavana@gmail.com>
2025-05-21 16:55:50 +0700

    login doang

--- loginAdmin.php
+++ loginAdmin.php
@@ -8,49 +8,57 @@
 </head>
 <body class="flex justify-center items-center min-h-screen m-6 bg-[#948979]">
     <div class="bg-[#DFD0B8] p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Login Admin</h2>
+        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6 mt-10">Login Admin</h2>
     
         <form action="#" method="POST" class="space-y-5">
           <!-- Username -->
-          <div>
-            <input type="text" id="username" name="username" placeholder="Username"
-              class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+          <div class="flex items-center border rounded-md mt-10 px-3 py-2 w-full focus-within:ring-2 focus-within:ring-blue-400">
+            <img src="./images/Group 1.png" alt="User Icon"  style="width: 17px; height: 17px;" class="mr-2" />
+            <input 
+                type="text" 
+                id="username" 
+                name="username" 
+                placeholder="Username"
+                class="w-full text-sm text-gray-700 placeholder-gray-400" />
           </div>
-    
-          <!-- Password -->
-          <div>
-            <input type="password" id="password" name="password" placeholder="Password"
-              class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+
+          <div class="flex items-center border rounded-md px-3 py-2 w-full focus-within:ring-2 focus-within:ring-blue-400">
+            <img src="./images/Group 3 (1).png" alt="User Icon" style="width: 17px; height: 17px;" class="mr-2" />
+            <input 
+                type="text" 
+                id="password" 
+                name="password" 
+                placeholder="Password"
+                class="w-full text-sm text-gray-700 placeholder-gray-400" />
+            <img src="./images/Group 5.png" alt="Password Icon" style="width: 17px; height: 17px;" />
           </div>
-    
-          <!-- Optional: Forgot password?-->
+
            <div class="flex items-center space-x-23 mt-1">
                 <div class="flex items-center space-x-2">
-            <input type="checkbox" id="remember" />
-            <label for="remember" class="text-sm text-gray-600">Remember me</label>
-            </div>
+                  <input type="checkbox" id="remember" />
+                  <label for="remember" class="text-sm text-gray-600">Remember me</label>
+                </div>
 
-            <p class="text-center text-sm text-gray-600">
-            Forgot password?
-            </p>
+                <p class="text-center text-sm text-gray-600">
+                Forgot password?
+                </p>
            </div>
 
            <!-- Sign In Button -->
-          <a href="dashboardAdmin.php" class="flex justify-center items-center w-full bg-[#393E46] text-white py-2 px-6 rounded-md hover:bg-[#2f3238] transition-colors">
+            <a href="dashboard.php" class="flex justify-center items-center w-full bg-[#393E46] text-white py-2 px-6 rounded-md hover:bg-[#2f3238] transition-colors">
             Login
             </a>
-               <!-- Anggota Login Option -->
+            <!-- Admin Login Option -->
             <div class="flex justify-center items-center mt-4 border-t pt-4">
                 <p class="text-center text-sm text-gray-700">
-                    Login Anggota
-                    <a href="loginAnggota.php" class="text-blue-500 font-medium ml-1">Klik disini</a>
+                    Login anggota
+                    <a href="loginAdmin.php" class="text-blue-500 font-medium ml-1">Klik disini</a>
                 </p>
             </div>
 
-           <!-- Optional: Link to register -->
             <p class="text-center text-sm text-gray-600">
-            Don’t have an account? 
-            <a href="register.html" class="text-blue-500 hover:underline">Register</a>
+                Don’t have an account? 
+                <a href="register.html" class="text-blue-500 hover:underline">Register</a>
             </p>
            </div>
         </form>
--- loginAnggota.php
+++ loginAnggota.php
@@ -8,12 +8,12 @@
 </head>
 <body class="flex justify-center items-center min-h-screen m-6 bg-[#948979]">
     <div class="bg-[#DFD0B8] p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Login Anggota</h2>
+        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6 mt-10">Login Anggota</h2>
     
         <form action="#" method="POST" class="space-y-5">
           <!-- Username -->
-          <div class="flex items-center border rounded-md px-3 py-2 w-full focus-within:ring-2 focus-within:ring-blue-400">
-            <img src="./images/Group 1.png" alt="User Icon" />
+          <div class="flex items-center border rounded-md mt-10 px-3 py-2 w-full focus-within:ring-2 focus-within:ring-blue-400">
+            <img src="./images/Group 1.png" alt="User Icon"  style="width: 17px; height: 17px;" class="mr-2" />
             <input 
                 type="text" 
                 id="username" 
@@ -23,14 +23,14 @@
           </div>
 
           <div class="flex items-center border rounded-md px-3 py-2 w-full focus-within:ring-2 focus-within:ring-blue-400">
-            <img src="./images/Group 3 (1).png" alt="User Icon" />
+            <img src="./images/Group 3 (1).png" alt="User Icon" style="width: 17px; height: 17px;" class="mr-2" />
             <input 
                 type="text" 
                 id="password" 
                 name="password" 
                 placeholder="Password"
                 class="w-full text-sm text-gray-700 placeholder-gray-400" />
-            <img src="./images/Group 5.png" alt="User Icon" />
+            <img src="./images/Group 5.png" alt="Password Icon" style="width: 17px; height: 17px;" />
           </div>
 
            <div class="flex items-center space-x-23 mt-1">
