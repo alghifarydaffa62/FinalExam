6d7a15bae7fdb12218de555094f96f6f355e6efe
M Daffa A <daffa.alghifary62@gmail.com>
2025-05-22 10:21:22 +0700

    edit direktori

--- Admin/loginAdmin.php
+++ Admin/loginAdmin.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./src/output.css" rel="stylesheet">
+    <link href="../src/output.css" rel="stylesheet">
     <title>Login Admin Page</title>
 </head>
 <body class="flex justify-center items-center min-h-screen m-6 bg-[#948979]">
     <div class="bg-[#DFD0B8] p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Login Admin</h2>
+        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Sign In Admin - SiPerpus</h2>
     
         <form action="#" method="POST" class="space-y-5">
           <div>
@@ -40,14 +40,9 @@
               <div class="flex justify-center items-center mt-4 border-t pt-4">
                   <p class="text-center text-sm text-gray-700">
                       Login Anggota ?
-                      <a href="loginAnggota.php" class="text-blue-500 font-medium hover:underline ml-1">Klik disini</a>
+                      <a href="../Anggota/loginAnggota.php" class="text-blue-500 font-medium hover:underline ml-1">Klik disini</a>
                   </p>
               </div>
-
-              <p class="text-center text-sm text-gray-700 mt-3">
-                Belum punya akun ? 
-                <a href="register.html" class="text-blue-500 font-medium hover:underline ml-1">Register</a>
-              </p>
           </div>
         </form>
     </div>
--- Anggota/dashboard.php
+++ Anggota/dashboard.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: loginAnggota.php");
-    exit;
-}
+// if (!isset($_SESSION['user_id'])) {
+//     header("Location: loginAnggota.php");
+//     exit;
+// }
 
 $user = [
     'name' => $_SESSION['user_name'] ?? 'Anggota',
