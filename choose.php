1c8b4a194ce7fc48d7c0f8429021ec44478067fc
piippp <rafifhadziq9@gmail.com>
2025-05-25 16:40:54 +0700

    change color icon choose.php

--- choose.php
+++ choose.php
@@ -10,19 +10,19 @@
 
 <body class="flex justify-center items-center min-h-screen m-6 bg-[#948979]">
     <div class="bg-[#DFD0B8] p-8 rounded-lg shadow-md w-full max-w-sm">
-        <h2 class="text-2xl font-bold text-center text-gray-700 mb-6">Sign In </h2>
+        <h2 class="text-2xl font-bold text-center text-white mb-6">Sign In </h2>
 
         <form action="#" method="POST" class="space-y-5">
             <div class="bg-[#948979] hover:bg-[#81786a] flex items-center border rounded-md px-3 py-2 w-full gap-2">
-                <img src="./images/peoples.png" alt="admin icon" class="w-5 h-5"/>
+                <img src="./images/people2.png" alt="admin icon" class="w-5 h-5"/>
                 <a href="./Anggota/loginAnggota.php"
-                    class="w-full text-sm text-gray-700 placeholder-gray-400 focus:outline-none border-none bg-transparent">
+                    class="w-full text-sm  text-white placeholder-gray-400 focus:outline-none border-none bg-transparent">
                     Sign In Anggota</a>
             </div>
             <div class="bg-[#948979] hover:bg-[#81786a] flex items-center border rounded-md px-3 py-2 w-full gap-2">
-                <img src="./images/admin.png" alt="admin icon" class="w-5 h-5"/>
+                <img src="./images/admin2.png" alt="admin icon" class="w-5 h-5"/>
                 <a href="./Admin/loginAdmin.php"
-                    class="w-full text-sm text-gray-700 placeholder-gray-400 focus:outline-none border-none bg-transparent">Sign In Admin</a>
+                    class="w-full text-sm text-white placeholder-gray-400 focus:outline-none border-none bg-transparent">Sign In Admin</a>
             </div>
         </form>
     </div>
