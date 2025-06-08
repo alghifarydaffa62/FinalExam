47ecc54d5240430c55efaf71e8682ec168b20b51
M Daffa A <daffa.alghifary62@gmail.com>
2025-06-08 15:30:48 +0700

    pliss work

--- konek.php
+++ konek.php
@@ -1,21 +1,22 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php'; 
+require_once __DIR__ . '/../vendor/autoload.php'; // naik 1 level ke root
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-$host = getenv("MYSQLHOST");
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
+$host = $_ENV["MYSQLHOST"] ?? getenv("MYSQLHOST");
+$user = $_ENV["MYSQLUSER"] ?? getenv("MYSQLUSER");
+$pass = $_ENV["MYSQLPASSWORD"] ?? getenv("MYSQLPASSWORD");
+$db   = $_ENV["MYSQLDATABASE"] ?? getenv("MYSQLDATABASE");
+$port = $_ENV["MYSQLPORT"] ?? getenv("MYSQLPORT");
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if(!$conn) {
     die("Koneksi gagal! " . mysqli_connect_error());
 }
-
 ?>
